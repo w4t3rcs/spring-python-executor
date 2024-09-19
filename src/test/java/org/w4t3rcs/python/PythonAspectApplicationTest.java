c2bf757d6dd33da61be1fe6b11f9b3a3bc2c1900
w4t3rcs <w4t3rofficial@gmail.com>
2024-09-19 10:43:53 +0200

    Some small edits

--- src/test/java/org/w4t3rcs/python/PythonAspectApplicationTest.java
+++ src/test/java/org/w4t3rcs/python/PythonAspectApplicationTest.java
@@ -3,7 +3,9 @@ package org.w4t3rcs.python;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.w4t3rcs.python.config.EnablePy4J;
 
+@EnablePy4J
 @SpringBootTest
 class PythonAspectApplicationTest {
     private final TestPythonScript testPythonScript;
