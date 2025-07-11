54b3b9c4e4de29c31162c876f5c6e1752f8c477f
w4t3rcs <w4t3rofficial@gmail.com>
2025-07-11 18:59:42 +0200

    Minor fixes

--- src/main/java/io/w4t3rcs/python/config/SpelythonProperties.java
+++ src/main/java/io/w4t3rcs/python/config/SpelythonProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @ConfigurationProperties("spring.python.spelython")
-public record SpelythonProperties(@DefaultValue("spel\\{.+}") String regex,
+public record SpelythonProperties(@DefaultValue("spel\\{.+?}") String regex,
                                   @DefaultValue("#") String spelLocalVariableIndex,
                                   @DefaultValue("5") int spelPositionFromStart,
                                   @DefaultValue("1") int spelPositionFromEnd) {
